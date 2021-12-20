5e8d19343cb76e10d2484c9317b138016f87032d
anopenscroll <mail@anopenscroll.com>
2021-12-20 10:21:04 -0500

    cleanup

--- src/CircularSlider/CircularSlider.tsx
+++ src/CircularSlider/CircularSlider.tsx
@@ -12,44 +12,11 @@ import { SetInitialKnobPosition } from "./Knob/Position/InitialKnobPosition";
 import { AdjustKnobPosition } from "./Knob/Position/KnobPosition";
 import { Initialize } from "./Initialize";
 import Paths from "./DrawPath/Paths/Paths";
-import { ICircularSliderProps } from "./Helpers/CircularSliderProps";
+import { GetProps, ICircularSliderProps } from "./Helpers/CircularSliderProps";
 import { HandleClickDragEvent } from "./Helpers/HandleClickDragEvent";
 
 function CircularSlider({ options }: { options: Partial<ICircularSliderProps> }) {
-    const props: ICircularSliderProps = {
-        label: options.label ?? "ANGLE",
-        width: options.width ?? 300,
-        direction: options.direction ?? 1,
-        min: options.min ?? 0,
-        max: options.max ?? 360,
-        knobColor: options.knobColor ?? "#4e63ea",
-        knobSize: options.knobSize ?? 36,
-        knobPosition: options.knobPosition ?? "top",
-        knobOffset: options.knobOffset ?? 0,
-        labelColor: options.labelColor ?? "#272b77",
-        labelBottom: options.labelBottom ?? false,
-        labelFontSize: options.labelFontSize ?? "1rem",
-        valueFontSize: options.valueFontSize ?? "3rem",
-        appendToValue: options.appendToValue ?? "",
-        prependToValue: options.prependToValue ?? "",
-        verticalOffset: options.verticalOffset ?? "1.5rem",
-        hideLabelValue: options.hideLabelValue ?? true,
-        hideKnob: options.hideKnob ?? false,
-        knobDraggable: options.knobDraggable ?? true,
-        progressColorFrom: options.progressColorFrom ?? "#80C3F3",
-        progressColorTo: options.progressColorTo ?? "#4990E2",
-        progressSize: options.progressSize ?? 24,
-        trackColor: options.trackColor ?? "#DDDEFB",
-        trackSize: options.trackSize ?? 24,
-        data: options.data ?? [],
-        dataIndex: options.dataIndex ?? 0,
-        progressLineCap: options.progressLineCap ?? "round",
-        renderLabelValue: options.renderLabelValue ?? null,
-        children: options.children ?? null,
-        onChange: options.onChange ?? ((value) => { }),
-        lockDashOffset: options.lockDashOffset ?? undefined,
-    };
-
+    const props: ICircularSliderProps = GetProps(options);
     const initialState: ICircularSliderState = Helpers.GetInitialState(props);
     const [state, dispatch] = useReducer(reducer, initialState);
 
--- src/CircularSlider/Helpers/CircularSliderProps.tsx
+++ src/CircularSlider/Helpers/CircularSliderProps.tsx
@@ -1,33 +1,69 @@
 export interface ICircularSliderProps {
-    label: string ;
-    width: number ;
-    direction: number ;
-    min: number ;
-    max: number ;
-    knobColor: string ;
-    knobSize: number ;
-    knobPosition: string ;
-    knobOffset: number ;
-    labelColor: string ;
-    labelBottom: boolean ;
-    labelFontSize: string ;
-    valueFontSize: string ;
-    appendToValue: string ;
-    prependToValue: string ;
-    verticalOffset: string ;
-    hideLabelValue: boolean ;
-    hideKnob: boolean ;
-    knobDraggable: boolean ;
-    progressColorFrom: string ;
-    progressColorTo: string ;
-    progressSize: number ;
-    trackColor: string ;
-    trackSize: number ;
-    data: never[] ;
-    dataIndex: number ;
-    progressLineCap: string ;
-    renderLabelValue: null ;
-    children: JSX.Element | null ;
-    onChange: ((value: any) => void) ;
-    lockDashOffset: number | undefined ;
+    label: string;
+    width: number;
+    direction: number;
+    min: number;
+    max: number;
+    knobColor: string;
+    knobSize: number;
+    knobPosition: string;
+    knobOffset: number;
+    labelColor: string;
+    labelBottom: boolean;
+    labelFontSize: string;
+    valueFontSize: string;
+    appendToValue: string;
+    prependToValue: string;
+    verticalOffset: string;
+    hideLabelValue: boolean;
+    hideKnob: boolean;
+    knobDraggable: boolean;
+    progressColorFrom: string;
+    progressColorTo: string;
+    progressSize: number;
+    trackColor: string;
+    trackSize: number;
+    data: never[];
+    dataIndex: number;
+    progressLineCap: string;
+    renderLabelValue: null;
+    children: JSX.Element | null;
+    onChange: ((value: any) => void);
+    lockDashOffset: number | undefined;
+}
+
+export function GetProps(options: Partial<ICircularSliderProps>): ICircularSliderProps {
+    return {
+        label: options.label ?? "ANGLE",
+        width: options.width ?? 300,
+        direction: options.direction ?? 1,
+        min: options.min ?? 0,
+        max: options.max ?? 360,
+        knobColor: options.knobColor ?? "#4e63ea",
+        knobSize: options.knobSize ?? 36,
+        knobPosition: options.knobPosition ?? "top",
+        knobOffset: options.knobOffset ?? 0,
+        labelColor: options.labelColor ?? "#272b77",
+        labelBottom: options.labelBottom ?? false,
+        labelFontSize: options.labelFontSize ?? "1rem",
+        valueFontSize: options.valueFontSize ?? "3rem",
+        appendToValue: options.appendToValue ?? "",
+        prependToValue: options.prependToValue ?? "",
+        verticalOffset: options.verticalOffset ?? "1.5rem",
+        hideLabelValue: options.hideLabelValue ?? true,
+        hideKnob: options.hideKnob ?? false,
+        knobDraggable: options.knobDraggable ?? true,
+        progressColorFrom: options.progressColorFrom ?? "#80C3F3",
+        progressColorTo: options.progressColorTo ?? "#4990E2",
+        progressSize: options.progressSize ?? 24,
+        trackColor: options.trackColor ?? "#DDDEFB",
+        trackSize: options.trackSize ?? 24,
+        data: options.data ?? [],
+        dataIndex: options.dataIndex ?? 0,
+        progressLineCap: options.progressLineCap ?? "round",
+        renderLabelValue: options.renderLabelValue ?? null,
+        children: options.children ?? null,
+        onChange: options.onChange ?? ((value) => { }),
+        lockDashOffset: options.lockDashOffset ?? undefined,
+    };
 }
